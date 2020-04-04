86b95063c518fb84189e129741c3f094dca99af8
paul.dewey@cdd.services <paul.m.dewey@gmail.com>
2020-04-04 22:08:10 +0100

    changed arrays.js to display a shopping list in arrays

---
+++ packages/core/playground/samples/arrays-BP.js
@@ -0,0 +1,76 @@
+module.exports = {
+  schema: {
+    type: "object",
+    title: "Company Details",
+    properties: {
+      name: {
+        type: "string",
+        title: "Name",
+        default: "",
+      },
+      company_number: {
+        type: "string",
+        title: "Company Number",
+        default: "",
+      },
+      previous_names: {
+        type: "array",
+        title: "Previous Names",
+        items: {
+          type: "object",
+          properties: {
+            company_name: { type: "string", title: "Company Name" },
+            start_date: { type: "string", title: "Start Date" },
+            end_date: { type: "string", title: "End Date" },
+          },
+        },
+      },
+      // source: {
+      //   type: "array",
+      //   title: "Source",
+      //   items: {
+      //     type: "string",
+      //     default: "",
+      //   },
+    },
+  },
+  uiSchema: {
+    previous_names: {
+      "ui:options": {
+        addable: false,
+        orderable: false,
+        removable: false,
+      },
+      items: {
+        "ui:options": {
+          addable: false,
+          orderable: false,
+          removable: false,
+        },
+        "ui:disabled": true,
+      },
+    },
+  },
+  formData: {
+    name: "BP P.L.C.",
+    company_number: "00102498",
+    previous_names: [
+      {
+        company_name: "BP AMOCO P.L.C.",
+        start_date: "1998-12-31",
+        end_date: "2001-05-01",
+      },
+      {
+        company_name: "THE BRITISH PETROLEUM COMPANY P.L.C.",
+        start_date: "1909-04-14",
+        end_date: "1998-12-31",
+      },
+    ],
+    // source: {
+    //   publisher: "UK Companies House",
+    //   url: "http://xmlgw.companieshouse.gov.uk/",
+    //   terms: "UK Crown Copyright",
+    //   retrieved_at: "2020-02-11T10:24:30+00:00"
+    // },
+  },
+};
--- packages/core/playground/samples/arrays.js
+++ packages/core/playground/samples/arrays.js
@@ -1,57 +1,188 @@
 module.exports = {
   schema: {
     type: "object",
-    title: "Company Details",
     properties: {
-      name: {
-        type: "string",
-        title: "Name",
-        default: "",
+      dairy: {
+        type: "array",
+        title: "Dairy",
+        items: {
+          type: "string",
+          default: "",
+        },
       },
-      company_number: {
-        type: "string",
-        title: "Company Number",
-        default: "",
+      grocery: {
+        type: "array",
+        title: "Grocery",
+        items: {
+          type: "string",
+          default: "",
+        },
       },
-      previous_names: 
-    	{ type: "array", title: "Previous Names", items: 
-    		{ type: "object", properties: 
-        	{ company_name: { type: "string", title: "Company Name" }, start_date: { type: "string", title: "Start Date" }, end_date: { type: "string", title: "End Date" } },
-        }
+      butcher: {
+        type: "array",
+        title: "Butcher",
+        items: {
+          type: "string",
+          default: "",
+        },
       },
+      // multipleChoicesList: {
+      //   type: "array",
+      //   title: "A multiple choices list",
+      //   items: {
+      //     type: "string",
+      //     enum: ["foo", "bar", "fuzz", "qux"],
+      //   },
+      //   uniqueItems: true,
+      // },
+      // fixedItemsList: {
+      //   type: "array",
+      //   title: "A list of fixed items",
+      //   items: [
+      //     {
+      //       title: "A string value",
+      //       type: "string",
+      //       default: "lorem ipsum",
+      //     },
+      //     {
+      //       title: "a boolean value",
+      //       type: "boolean",
+      //     },
+      //   ],
+      //   additionalItems: {
+      //     title: "Additional item",
+      //     type: "number",
+      //   },
+      // },
+      // minItemsList: {
+      //   type: "array",
+      //   title: "A list with a minimal number of items",
+      //   minItems: 3,
+      //   items: {
+      //     $ref: "#/definitions/Thing",
+      //   },
+      // },
+      // defaultsAndMinItems: {
+      //   type: "array",
+      //   title: "List and item level defaults",
+      //   minItems: 5,
+      //   default: ["carp", "trout", "bream"],
+      //   items: {
+      //     type: "string",
+      //     default: "unidentified",
+      //   },
+      // },
+      // nestedList: {
+      //   type: "array",
+      //   title: "Nested list",
+      //   items: {
+      //     type: "array",
+      //     title: "Inner list",
+      //     items: {
+      //       type: "string",
+      //       default: "lorem ipsum",
+      //     },
+      //   },
+      // },
+      // unorderable: {
+      //   title: "Unorderable items",
+      //   type: "array",
+      //   items: {
+      //     type: "string",
+      //     default: "lorem ipsum",
+      //   },
+      // },
+      // unremovable: {
+      //   title: "Unremovable items",
+      //   type: "array",
+      //   items: {
+      //     type: "string",
+      //     default: "lorem ipsum",
+      //   },
+      // },
+      // noToolbar: {
+      //   title: "No add, remove and order buttons",
+      //   type: "array",
+      //   items: {
+      //     type: "string",
+      //     default: "lorem ipsum",
+      //   },
+      // },
+      // fixedNoToolbar: {
+      //   title: "Fixed array without buttons",
+      //   type: "array",
+      //   items: [
+      //     {
+      //       title: "A number",
+      //       type: "number",
+      //       default: 42,
+      //     },
+      //     {
+      //       title: "A boolean",
+      //       type: "boolean",
+      //       default: false,
+      //     },
+      //   ],
+      //   additionalItems: {
+      //     title: "A string",
+      //     type: "string",
+      //     default: "lorem ipsum",
+      //   },
+      // },
     },
   },
   uiSchema: {
-    previous_names: {
-      "ui:options": {
-        addable: false,
-        orderable: false,
-        removable: false,
-      },
-      items: {
-        "ui:options": {
-          addable: false,
-          orderable: false,
-          removable: false,
-        },
-        "ui:disabled": true,
-      },
+    dairy: {
+      items: { "ui:emptyValue": "" },
     },
+    grocery: {
+      items: { "ui:emptyValue": "" },
+    },
+    butcher: {
+      items: { "ui:emptyValue": "" },
+    },
+    // multipleChoicesList: {
+    //   "ui:widget": "checkboxes",
+    // },
+    // fixedItemsList: {
+    //   items: [{ "ui:widget": "textarea" }, { "ui:widget": "select" }],
+    //   additionalItems: {
+    //     "ui:widget": "updown",
+    //   },
+    // },
+    // unorderable: {
+    //   "ui:options": {
+    //     orderable: false,
+    //   },
+    // },
+    // unremovable: {
+    //   "ui:options": {
+    //     removable: false,
+    //   },
+    // },
+    // noToolbar: {
+    //   "ui:options": {
+    //     addable: false,
+    //     orderable: false,
+    //     removable: false,
+    //   },
+    // },
+    // fixedNoToolbar: {
+    //   "ui:options": {
+    //     addable: false,
+    //     orderable: false,
+    //     removable: false,
+    //   },
+    // },
   },
   formData: {
-    name: "BP P.L.C.",
-    company_number: "00102498",
-    previous_names: [
-      {
-        company_name: "BP AMOCO P.L.C.",
-        start_date: "1998-12-31",
-        end_date: "2001-05-01"
-      },
-      {
-        company_name: "THE BRITISH PETROLEUM COMPANY P.L.C.",
-        start_date: "1909-04-14",
-        end_date: "1998-12-31"
-      }
-      ],    
+    dairy: ["Milk", "Cheese"],
+    // multipleChoicesList: ["foo", "bar"],
+    // fixedItemsList: ["Some text", true, 123],
+    // nestedList: [["lorem", "ipsum"], ["dolor"]],
+    // unorderable: ["one", "two"],
+    // unremovable: ["one", "two"],
+    // noToolbar: ["one", "two"],
+    // fixedNoToolbar: [42, true, "additional item one", "additional item two"],
   },
 };
